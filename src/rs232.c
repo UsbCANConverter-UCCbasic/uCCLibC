7f467c4d3b5b4b2bd4a7b221c1a9509ffc546ce4
Clovis Durand <cd.clovel19@gmail.com>
2020-03-21 19:38:21 +0100

    Fixed writable-strings warnings for ISO C++11 compilation
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/rs232.c
+++ src/rs232.c
@@ -38,21 +38,52 @@
 
 #define RS232_PORTNR  38
 
-
 int Cport[RS232_PORTNR],
     error;
 
 struct termios new_port_settings,
        old_port_settings[RS232_PORTNR];
 
-char *comports[RS232_PORTNR]={"/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyS5",
-                       "/dev/ttyS6","/dev/ttyS7","/dev/ttyS8","/dev/ttyS9","/dev/ttyS10","/dev/ttyS11",
-                       "/dev/ttyS12","/dev/ttyS13","/dev/ttyS14","/dev/ttyS15","/dev/ttyUSB0",
-                       "/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4","/dev/ttyUSB5",
-                       "/dev/ttyAMA0","/dev/ttyAMA1","/dev/ttyACM0","/dev/ttyACM1",
-                       "/dev/rfcomm0","/dev/rfcomm1","/dev/ircomm0","/dev/ircomm1",
-                       "/dev/cuau0","/dev/cuau1","/dev/cuau2","/dev/cuau3",
-                       "/dev/cuaU0","/dev/cuaU1","/dev/cuaU2","/dev/cuaU3"};
+const char *comports[RS232_PORTNR]={
+    "/dev/ttyS0",
+    "/dev/ttyS1",
+    "/dev/ttyS2",
+    "/dev/ttyS3",
+    "/dev/ttyS4",
+    "/dev/ttyS5",
+    "/dev/ttyS6",
+    "/dev/ttyS7",
+    "/dev/ttyS8",
+    "/dev/ttyS9",
+    "/dev/ttyS10",
+    "/dev/ttyS11",
+    "/dev/ttyS12",
+    "/dev/ttyS13",
+    "/dev/ttyS14",
+    "/dev/ttyS15",
+    "/dev/ttyUSB0",
+    "/dev/ttyUSB1",
+    "/dev/ttyUSB2",
+    "/dev/ttyUSB3",
+    "/dev/ttyUSB4",
+    "/dev/ttyUSB5",
+    "/dev/ttyAMA0",
+    "/dev/ttyAMA1",
+    "/dev/ttyACM0",
+    "/dev/ttyACM1",
+    "/dev/rfcomm0",
+    "/dev/rfcomm1",
+    "/dev/ircomm0",
+    "/dev/ircomm1",
+    "/dev/cuau0",
+    "/dev/cuau1",
+    "/dev/cuau2",
+    "/dev/cuau3",
+    "/dev/cuaU0",
+    "/dev/cuaU1",
+    "/dev/cuaU2",
+    "/dev/cuaU3"
+};
 
 int RS232_OpenComport(int comport_number, int baudrate, const char *mode)
 {
